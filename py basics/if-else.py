9e786b0bc7f154aec2ad247aa861da4987a1e7e9
Yuvraj Singh <yuwe18b@gmail.com>
2023-09-25 23:29:45 +0530

    feat: add new file for tuples and modify

---
+++ py basics/if-else.py
@@ -0,0 +1,13 @@
+x=2
+y=4
+z=6
+if x<y:
+    print("x is less than y")   
+elif x>z:
+    print("x is greater than z")    
+else:
+    print("x is greater than y")
+    print("x is less than z")
+    print("x is less than y")
+
+# In[ ]:
---
+++ py basics/loops.py
@@ -0,0 +1,13 @@
+# write loops program on python to understand the concept of loops in python 
+
+# In[1]:
+
+x=1
+y=10
+for i in range(x,y):
+    print(i)
+while x<y:
+    print(x)
+    x+=1
+    
+
