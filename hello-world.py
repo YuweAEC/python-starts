091ddb51bf6b7b520495f2f50800ad77cb778405
Yuvraj Singh <yuwe18b@gmail.com>
2023-10-07 23:46:21 +0530

    refactor: add new code for finding cp using loops and pow functions

--- hello-world.py
+++ hello-world.py
@@ -15,3 +15,15 @@ compound_interest(10000, 10.25, 5)
 
 
 
+
+# Python3 program to find compound
+# interest for input taking from user.
+
+
+def compound_interest(principal, rate, time):
+
+	# Calculates compound interest
+	Amount = principal * (pow((1 + rate / 100), time))
+	CI = Amount - principal
+	print("Compound interest is", CI)
+
