223c96d1481c087b3167803e32c2399dbfe1a90f
Yuvraj Singh <yuwe18b@gmail.com>
2023-10-07 23:30:16 +0530

    feat: add new file for cp using simple code

--- hello-world.py
+++ hello-world.py
@@ -1,2 +1,17 @@
-print("hello world")
-print("Yuvi invites you to the world of python")
+# Python3 program to find compound
+# interest for given values.
+
+
+def compound_interest(principal, rate, time):
+
+	# Calculates compound interest
+	Amount = principal * (pow((1 + rate / 100), time))
+	CI = Amount - principal
+	print("Compound interest is", CI)
+
+
+# Driver Code
+compound_interest(10000, 10.25, 5)
+
+
+
