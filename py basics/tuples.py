9644bf038712d257e8466794038a4985adbfd3ba
Yuvraj Singh <yuwe18b@gmail.com>
2023-11-05 22:31:52 +0530

    feat: add new code

---
+++ py basics/tuples.py
@@ -0,0 +1,30 @@
+# write simple tuple code here to understand the concept of tuples in python 
+
+import sys
+import os
+
+# tuple is immutable
+# tuple is faster than list
+# tuple is used for data that does not change
+
+# tuple is defined by using () brackets
+
+# tuple can be defined with or without () brackets
+tup1 = 1, 2, 3, 4, 5
+tup2 = (1, 2, 3, 4, 5)
+
+print(tup1)
+print(tup2)
+
+# tuple can be defined with or without () brackets
+tup3 = 1,
+tup4 = (1,)
+print(tup3)
+print(tup4)
+
+# tuple can be defined with or without () brackets
+tup5 = 1, 2, 3, 4, 5
+tup6 = (1, 2, 3, 4, 5)
+print(tup5)
+print(tup6)
+
